ae8eaabd140d90d0844ffc7f6af9ed5434ce66dc
Muhammad Salman <slmnkhan2050@gmail.com>
2024-03-17 08:41:53 +0500

    Push, Pull, shift and unshift

---
+++ app.js
@@ -0,0 +1,32 @@
+// variables making
+
+var namesOfTopics = ['html', 'CSS', 'JS', 'TS', 'React']
+
+console.log(namesOfTopics)
+
+// less last word
+
+namesOfTopics.pop()
+
+console.log(namesOfTopics)
+
+// create last word
+
+namesOfTopics.push('C++')
+
+console.log(namesOfTopics)
+
+// less value from start
+
+namesOfTopics.shift()
+
+console.log(namesOfTopics)
+
+// add addition from start
+
+namesOfTopics.unshift('html')
+
+console.log(namesOfTopics)
+
+// Add via order
+
